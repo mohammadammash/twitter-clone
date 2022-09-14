8769c16cceb14ffd0aa02af6148c488856e1bcb0
mohammadammash <mohammadammash31@gmail.com>
2022-09-14 20:07:04 +0300

    Adding two event listeners to close specific modal(login or signup) when clicked on close(x) top-right button in '/frontend/js/index.js'

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,10 +1,13 @@
 const login_modal = document.getElementById('login-modal');
 const login_submit_btn = document.getElementById("login-submit");
 const login_show_btn = document.getElementById("login-show");
+const close_login = document.getElementById("close-login");
+
 const signup_modal = document.getElementById('signup-modal');
 const signup_submit_btn = document.getElementById('signup-submit');
 const signup_show_btn = document.getElementById("signup-show");
 const signup_show_link = document.getElementById('signup-link');
+const close_signup = document.getElementById('close-signup');
 
 // SIGN UP FUNCTIONS
 const showSignupModal = ()=>{
@@ -32,7 +35,8 @@ const loginUser = (e) => {
 signup_show_btn.addEventListener('click',showSignupModal);
 signup_show_link.addEventListener("click", showSignupModal);
 signup_submit_btn.addEventListener('click',createNewUser);
+close_signup.addEventListener("click", () =>signup_modal.classList.add("display-none"));
 // LOGIN EVENT LISTENERS
 login_show_btn.addEventListener("click", showLoginModal);
 login_submit_btn.addEventListener("click", loginUser);
-
+close_login.addEventListener("click", () =>login_modal.classList.add("display-none"));
