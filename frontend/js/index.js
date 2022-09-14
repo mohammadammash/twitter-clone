2db059e0881558b09d46112adcb921c6e7aae5c6
mohammadammash <mohammadammash31@gmail.com>
2022-09-14 19:53:41 +0300

    Adding two event listener to handle the signup-modul showing when requested in '/frontend/js/index.js'

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -5,3 +5,13 @@ const signup_modal = document.getElementById('signup-modal');
 const signup_submit_btn = document.getElementById('signup-submit');
 const signup_show_btn = document.getElementById("signup-show");
 const signup_show_link = document.getElementById('signup-link');
+
+const showSignupModal = ()=>{
+    console.log('HEYYY')
+    // make sure login-modal is hidden(add display-none) - hence we can go directly from login-modal to sign-up modal 
+    login_modal.classList.add('display-none'); //note: it have no effect if class already exists
+    signup_modal.classList.remove('display-none');
+}
+
+signup_show_btn.addEventListener('click',showSignupModal);
+signup_show_link.addEventListener("click", showSignupModal);
