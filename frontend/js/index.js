916fd315bfcc3b1407bf6d7c021ff98a67241844
mohammadammash <mohammadammash31@gmail.com>
2022-09-14 18:44:42 +0300

    Fixing naming error in '/frontend/index.html' and adding basic selection in 'js/index.js'

---
+++ frontend/js/index.js
@@ -0,0 +1,7 @@
+const login_modal = document.getElementById('login-modal');
+const login_submit_btn = document.getElementById("login-submit");
+const login_show_btn = document.getElementById("login-show");
+const signup_modal = document.getElementById('signup-modal');
+const signup_submit_btn = document.getElementById('signup-submit');
+const signup_show_btn = document.getElementById("signup-show");
+const signup_show_link = document.getElementById('signup-link');
