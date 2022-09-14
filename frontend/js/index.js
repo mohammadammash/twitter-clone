e338ca92c82f0d74bf9a40c860fce916c1078165
mohammadammash <mohammadammash31@gmail.com>
2022-09-14 20:02:19 +0300

    Adding two event listeners to handle the submit of signup and login forms and prevent default event in '/frontend/js/index.js'

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -6,19 +6,33 @@ const signup_submit_btn = document.getElementById('signup-submit');
 const signup_show_btn = document.getElementById("signup-show");
 const signup_show_link = document.getElementById('signup-link');
 
+// SIGN UP FUNCTIONS
 const showSignupModal = ()=>{
     // make sure login-modal is hidden(add display-none) - hence we can go directly from login-modal to sign-up modal 
     login_modal.classList.add('display-none'); //note: it have no effect if class already exists
     signup_modal.classList.remove('display-none');
 }
+    // handle signup form submit
+const createNewUser = (e)=>{
+    e.preventDefault();
+}
 
+// LOGIN FUNCTIONS
 const showLoginModal = ()=>{
     // there is no way to access login from signup page => no need to hide signup-modal hence it is already hidden
     login_modal.classList.remove('display-none')
 }
+    // handle login form submit
+const loginUser = (e) => {
+  e.preventDefault();
+};
 
-// SignUp Event Listeners
+
+// SIGNUP EVENT LISTENERS
 signup_show_btn.addEventListener('click',showSignupModal);
 signup_show_link.addEventListener("click", showSignupModal);
-// Login Event Listeners
+signup_submit_btn.addEventListener('click',createNewUser);
+// LOGIN EVENT LISTENERS
 login_show_btn.addEventListener("click", showLoginModal);
+login_submit_btn.addEventListener("click", loginUser);
+
