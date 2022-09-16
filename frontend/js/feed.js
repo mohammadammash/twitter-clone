b1155ec2fbe251920038935bb1b5cefc42be3baf
mohammadammash <mohammadammash31@gmail.com>
2022-09-16 08:53:01 +0300

    Adding ids necessary to fetch and interact with to add a new tweet, with creating needed js file: '/frontend/js/feed.js' and selecting those tags

---
+++ frontend/js/feed.js
@@ -0,0 +1,9 @@
+const newtweet_text = document.getElementById('newtweet-text');
+const newtweet_imgurl = document.getElementById('newtweet-imgurl');
+const newtweet_submit = document.getElementById('newtweet-submit');
+const newtweet_imgshow = document.getElementById('newtweet-imgshow');
+
+console.log(newtweet_text);
+console.log(newtweet_imgurl);
+console.log(newtweet_submit);
+console.log(newtweet_imgshow);
