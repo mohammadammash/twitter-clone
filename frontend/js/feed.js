68dad0f0e0a7b373177a12e57f893e0a0ad739b4
mohammadammash <mohammadammash31@gmail.com>
2022-09-16 09:32:35 +0300

    adding getTodayDate() to find the date which the post is created at in '/frontend/js/feed.js'

--- frontend/js/feed.js
+++ frontend/js/feed.js
@@ -4,7 +4,8 @@ const newtweet_submit = document.getElementById('newtweet-submit');
 const newtweet_imgshow = document.getElementById('newtweet-imgshow');
 var base64String;
 
-// show image and save url
+
+// show image and save url (didn't use arrow function => 'this' doen't work well inside)
 function uploadImage() {
   if (this.files && this.files[0]) {
     var reader = new FileReader();
@@ -17,6 +18,21 @@ function uploadImage() {
   }
 }
 
+// find today's date as dd/mm/yyyy:
+const getTodayDate = () => {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1;
+  var yyyy = today.getFullYear();
+  if (dd < 10) dd = "0" + dd;
+  if (mm < 10) mm = "0" + mm;
+  return dd + "/" + mm + "/" + yyyy;
+};
+
+
+
+
 // whenever we change the image we add, recreate base64 and show the new image
 newtweet_imgurl.addEventListener("change", uploadImage);
-
+// submit the new tweet
+// newtweet_submit.addEventListener('click',addNewTweet);
