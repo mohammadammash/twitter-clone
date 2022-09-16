51d339803f4442c0808b55cf6ffb31c24a9b9659
mohammadammash <mohammadammash31@gmail.com>
2022-09-16 09:01:51 +0300

    obtain the new tweet uploaded image base64 result, show the image in the same tweet container

--- frontend/js/feed.js
+++ frontend/js/feed.js
@@ -2,8 +2,21 @@ const newtweet_text = document.getElementById('newtweet-text');
 const newtweet_imgurl = document.getElementById('newtweet-imgurl');
 const newtweet_submit = document.getElementById('newtweet-submit');
 const newtweet_imgshow = document.getElementById('newtweet-imgshow');
+var base64String;
 
-console.log(newtweet_text);
-console.log(newtweet_imgurl);
-console.log(newtweet_submit);
-console.log(newtweet_imgshow);
+// show image and save url
+function uploadImage() {
+  if (this.files && this.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      base64String = reader.result.replace("data:", "").replace(/^.+,/, "");
+      newtweet_imgshow.src = e.target.result;
+      newtweet_imgshow.classList.remove('display-none');
+    };
+    reader.readAsDataURL(this.files[0]);
+  }
+}
+
+// whenever we change the image we add, recreate base64 and show the new image
+newtweet_imgurl.addEventListener("change", uploadImage);
+
