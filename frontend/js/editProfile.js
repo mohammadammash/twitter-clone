0d2e06fa009bb079c2644e3a3f00b2d94019599a
jessymatta <jessy.w.matta@gmail.com>
2022-09-17 21:06:02 +0300

    added a function to apply changes of the edited profile to the db

--- frontend/js/editProfile.js
+++ frontend/js/editProfile.js
@@ -1,15 +1,15 @@
-// To open and close the Edit profile Modal
+// Edit profile Modal
 const modalBtn = document.getElementById('edit-p');
 const modalBg = document.getElementById('modal-bg');
 const modalClose = document.getElementById('close-edit-profile');
+// To replace the contents with the modified data
+const newtweet_text = document.getElementById("newtweet-text"); //name
+const newtweet_imgurl = document.getElementById("newtweet-imgurl"); //bio
+const newtweet_submit = document.getElementById("newtweet-submit"); //pp
+const newtweet_imgshow = document.getElementById("newtweet-imgshow"); //banner
 
-modalBtn.addEventListener('click', function () {
-    modalBg.classList.add('bg-active');
-})
-
-modalClose.addEventListener('click', function () {
-    modalBg.classList.remove('bg-active');
-})
+var base64String;
+const feed_content = document.getElementById("feed-content");
 
 
 // On window load => check if page is in frame or without parent location
@@ -20,4 +20,23 @@ const checkIfPageIsInFrame = () => {
     // if the page is in an iframe continue normally
 };
 
+
+// Edit Profile Modal EventListeners
+modalBtn.addEventListener('click', function () {
+    modalBg.classList.add('bg-active');
+})
+modalClose.addEventListener('click', function () {
+    modalBg.classList.remove('bg-active');
+})
+
+
+
+
+//now need to apply name and bio changes to the profile section
+
+
+
+
+// each time page loads, check if the user tries to access the page without partials page(outside of frame=>redirect to partials)
+window.addEventListener("load", checkIfPageIsInFrame);
 
