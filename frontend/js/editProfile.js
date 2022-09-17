77c0a76161cf56a0da00e37eeb58765f252f7fe1
jessymatta <jessy.w.matta@gmail.com>
2022-09-17 13:17:13 +0300

    a function to check if the profile page is in frame or without parent location

--- frontend/js/editProfile.js
+++ frontend/js/editProfile.js
@@ -1,13 +1,23 @@
 // To open and close the Edit profile Modal
 const modalBtn = document.getElementById('edit-p');
-const modalBg=document.getElementById('modal-bg');
-const modalClose=document.getElementById('close-edit-profile');
+const modalBg = document.getElementById('modal-bg');
+const modalClose = document.getElementById('close-edit-profile');
 
-modalBtn.addEventListener('click', function(){
+modalBtn.addEventListener('click', function () {
     modalBg.classList.add('bg-active');
 })
 
-modalClose.addEventListener('click', function(){
+modalClose.addEventListener('click', function () {
     modalBg.classList.remove('bg-active');
 })
+
+
+// On window load => check if page is in frame or without parent location
+const checkIfPageIsInFrame = () => {
+    // The page is not in an iframe => redirect to /partials.html
+    if (window.location === window.parent.location)
+        window.location.href = "./partials.html";
+    // if the page is in an iframe continue normally
+};
+
 
