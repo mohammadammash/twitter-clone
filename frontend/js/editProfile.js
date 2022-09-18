38edd0006ee03285a97ea0f00dfe2d79141c1948
jessymatta <jessy.w.matta@gmail.com>
2022-09-18 09:52:42 +0300

    displayed original info on the screen when the profile page is first opened in 'editProfile.js

--- frontend/js/editProfile.js
+++ frontend/js/editProfile.js
@@ -5,6 +5,9 @@ const modalClose = document.getElementById('close-edit-profile');
 const save_changes = document.getElementById('save-changes');
 const edited_name = document.getElementById('edited-name');//name
 const edited_bio = document.getElementById('edited-bio');//bio
+const name_on_page = document.getElementById('name-on-page');
+const bio_on_page = document.getElementById('bio-on-page');
+
 // console.log("user in local storage " + JSON.parse(localStorage.getItem("user")));
 
 
@@ -26,37 +29,33 @@ modalClose.addEventListener('click', function () {
 
 
 //A function that retrieves user info from db and displays them in the input fields of the modal
-//!document.getElementById("nameofid").value = "My value";
 
-    //to change user_id = 1, to the below syntax since user's info is not showing on my device's local storage
-    // const user = JSON.parse(localStorage.getItem("user"));
+//to change user_id = 1, to the below syntax since user's info is not showing on my device's local storage
+// const user = JSON.parse(localStorage.getItem("user"));
 
-    const changeInputValue = async () => {
-        try {
-            // const url = `http://localhost/twitter-clone/backend/user_info.php?user_id=${user.id}`;
-            const url = `http://localhost/twitter-clone/backend/user_info.php?user_id=1`;
-            const response = await fetch(url);
-            const data = await response.json();
-            console.log(data);
-            const original_name=data[0].full_name;
-            console.log(original_name);
-            const original_bio=data[0].bio;
-            console.log(original_bio);
-            const original_pp=data[0].banner;
-            console.log(original_pp);
-            const original_banner=data[0].avatar_url;
-            console.log(original_banner);
+const changeInputValue = async () => {
+    try {
+        // const url = `http://localhost/twitter-clone/backend/user_info.php?user_id=${user.id}`;
+        const url = `http://localhost/twitter-clone/backend/user_info.php?user_id=1`;
+        const response = await fetch(url);
+        const data = await response.json();
+        const original_name = data[0].full_name;
+        const original_bio = data[0].bio;
+        const original_pp = data[0].banner;
+        const original_banner = data[0].avatar_url;
 
-            document.getElementById("edited-name").value = original_name;
-            document.getElementById("edited-bio").value = original_bio;
+        name_on_page.innerHTML=original_name;
+        bio_on_page.innerHTML=original_bio;
+        edited_name.value = original_name;
+        edited_bio.value = original_bio;
 
 
-        } catch (err) {
-            console.log(err);
-        }
-    };
+    } catch (err) {
+        console.log(err);
+    }
+};
 
-    changeInputValue();
+changeInputValue();
 
 
 
@@ -99,8 +98,19 @@ let getNameBioFrInput = (e) => {
         } catch (err) {
             console.log(err);
         }
+
+
     };
     update_db();
+
+    //To display the updated info on the page
+    const update_page_info = () => {
+        name_on_page.innerHTML = full_name;
+        bio_on_page.innerHTML = bio;
+
+    }
+
+    update_page_info();
 
 };
 
